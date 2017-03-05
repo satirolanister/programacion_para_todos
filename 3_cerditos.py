5c77720f3e4e6bad6d480b41c13792030382bda3
airetupal <icamargo.ingeniero@gmail.com>
2017-03-05 12:03:45 -0500

    Cod_Funcional
    Cod_Funcional

--- 3_cerditos.py
+++ 3_cerditos.py
@@ -13,3 +13,18 @@ elif velocidad_viento < 33.4:
 else:
     print("Es ladrillo")
  #termina proyecto
+
+#---------------------------------------------------------------------------------------------------
+>>> import math
+material_paja = float(3.2)
+material_madera = float(33.4)
+material_ladrillo = float(150)
+Vel_Viento = int(input("Ingrese la velocidad con la que el lobo sopla: "))
+>>> if Vel_Viento < 3.2:
+    print("El lobo puede tumbar la casa hecha de paja")
+elif Vel_Viento < 33.4:
+    print("El lobo puede tumbar la casa hecha de paja y madera")
+elif velocidad_viento <= 150:
+    print("El lobo puede tumbar la casa hecha de paja, madera y ladrillo")
+else:
+    print("El lobo no puede tirar la casa")
